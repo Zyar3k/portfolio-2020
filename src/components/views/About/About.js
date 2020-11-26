4bcec55e5327c89f667b4b1708dfc729257cd529
Arkadiusz Zygan <a.zygan@gmail.com>
2020-11-26 20:20:01 +0100

    Add about section

---
+++ src/components/views/About/About.js
@@ -0,0 +1,58 @@
+import "./About.scss";
+
+const About = () => {
+  return (
+    <section id='about' className='pageSection'>
+      <section className='pageContent container aboutContent'>
+        <main>
+          <section className='aboutSection'>
+            <span className='icoWrapper'>
+              <i className='fas fa-laptop-code' />
+            </span>
+            <p>
+              Nazywam się Arkadiusz Zygan. W 2019 roku postanowiłem
+              usystematyzować wiedzę z zakresu kodowania i programowania. Na
+              przestrzeni lat interesowałem się nowymi technologiami, jednak nie
+              uzyskałem pełnej wiedzy teoretycznej, jak i nie zrealizowałem
+              praktycznych projektów. Od tamtego czasu systematycznie uczę się
+              programowania po stronie front-endu z elementami back-endu.
+            </p>
+          </section>
+          <section className='aboutSection'>
+            <span className='icoWrapper'>
+              <i className='fab fa-leanpub' />
+            </span>
+            <p>
+              Ukończyłem intensywny 9-miesięczny kurs programowania pod opieką
+              indywidualnego mentora – bootcamp web developer plus w Kodilla.
+              Uczestniczyłem również w szkoleniach rozszerzających moje
+              umiejętności na platformie udemy z zakresu javascript, react i
+              data science. Na bieżąco obserwuje zmiany w trendach, posiłkując
+              się wieloma kanałami tematycznymi na youtube (m.in. Dev Ed -
+              bardzo duża wiedza i zaraźliwy optymizm autora, Web Dev Simplified
+              - 'kopalnia' wiedzy, DesignCourse - fachowo i konkretnie na temat
+              designu).
+            </p>
+          </section>
+          <section className='aboutSection'>
+            <span className='icoWrapper'>
+              <i className='fas fa-infinity' />
+            </span>
+            <p>
+              'praktyka czyni mistrza' - każdy z nas to słyszał chociaż raz w
+              swoim życiu. Zgadzam się, ale uważam, że trzeba dodać 'świadome
+              ćwiczenie' - termin ten wykuł anders erics­son. Uważa on również,
+              że aby osiągnąć wysoki poziom w dowolnej dziedzinie potrzeba nawet
+              10000 godzin. Zdaję sobię sprawę, że wciąż dużo pracy przede mną.
+              Ćwiczę i rozwijam swoje umiejętności w javascript, react, html5
+              oraz css/sass. Wiem, że jestem na początku tej drogi, ale jestem
+              zmotywowany, aby stawiać kolejne kroki.
+            </p>
+          </section>
+        </main>
+      </section>
+    </section>
+  );
+};
+
+export default About;
--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -3,12 +3,13 @@ import "./Home.scss";
 const Home = () => {
   return (
     <section id='start' className='pageSection'>
-      <section className='pageContent homeContent'>
+      <section className='pageContent container homeContent'>
         <main>
           <h1>ZYGAN AREK</h1>
           <h4>FRONTEND DEVELOPER</h4>
         </main>
       </section>
+      <a href='#about' className='btnNext far fa-arrow-alt-circle-down' />
     </section>
   );
 };
