53a672c48a426fe6135456bebe97ebd22ec7317e
Arkadiusz Zygan <a.zygan@gmail.com>
2020-11-28 02:18:32 +0100

    Edit sidebar ux

--- src/components/features/Sidebar/Sidebar.js
+++ src/components/features/Sidebar/Sidebar.js
@@ -4,18 +4,23 @@ const Sidebar = () => {
   return (
     <nav>
       <a href='#start'>
+        <p>start</p>
         <i className='far fa-star' />
       </a>
       <a href='#about'>
+        <p>about</p>
         <i className='far fa-address-card'></i>
       </a>
       <a href='#skills'>
+        <p>skills</p>
         <i className='fas fa-star-half-alt'></i>
       </a>
       <a href='#portfolio'>
+        <p>portfolio</p>
         <i className='fas fa-briefcase'></i>
       </a>
       <a href='#contact'>
+        <p>contact</p>
         <i className='fas fa-envelope'></i>
       </a>
     </nav>
